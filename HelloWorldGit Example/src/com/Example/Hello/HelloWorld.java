0be7c7af00be7399fd9c7af9f86a07200866e51c
Rajeev <softdevrajeeev@gmail.com>
2013-11-13 08:36:07 +0530

    First Commit To check

---
+++ HelloWorldGit Example/src/com/Example/Hello/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.Example.Hello;
+
+public class HelloWorld {
+
+	public void Display(){
+		System.out.println("Hello World Class");
+	}
+	
+}
---
+++ HelloWorldGit Example/src/com/Example/Hello/Main.java
@@ -0,0 +1,19 @@
+package com.Example.Hello;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HelloWorld hw = new HelloWorld();
+		
+		hw.Display();
+		Display();
+		System.out.println("Hello World Main Class");
+
+	}
+	
+	public static void Display(){
+		System.out.println("Hello World Main Class");
+	}
+
+}
